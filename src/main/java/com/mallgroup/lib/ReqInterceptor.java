49096bbd11518b2ba5eab969da8f4c511172ab3e
Jiri Sobotik <jiri.sobotik@actum.cz>
2018-10-11 16:19:17 +0200

    print all headers

--- src/main/java/com/mallgroup/lib/ReqInterceptor.java
+++ src/main/java/com/mallgroup/lib/ReqInterceptor.java
@@ -4,11 +4,17 @@ import org.wso2.msf4j.Request;
 import org.wso2.msf4j.Response;
 import org.wso2.msf4j.interceptor.RequestInterceptor;
 
+import java.util.stream.Stream;
+
 public class ReqInterceptor implements RequestInterceptor {
 
     @Override
     public boolean interceptRequest(Request request, Response response) throws Exception {
-        System.out.println(request.getUri());
+
+        // print all request headers
+        Stream.of(request.getHeaders().getRequestHeaders().values().toArray())
+                .forEach(System.out::println);
+
         return true;
     }
 }
--- src/main/java/com/mallgroup/producer/Application.java
+++ src/main/java/com/mallgroup/producer/Application.java
@@ -8,19 +8,8 @@ import org.wso2.msf4j.spring.SpringMicroservicesRunner;
 public class Application {
 
     public static void main(String[] args) {
-//        MSF4JSpringApplication application = new MSF4JSpringApplication(Application.class);
-//        application.addService()
         ConfigurableApplicationContext context = MSF4JSpringApplication.run(Application.class, args);
-        context.getBean(SpringMicroservicesRunner.class).addGlobalRequestInterceptor(new ReqInterceptor());
-        //.run(Application.class, args);
-
-
-        //msf4JSpringApplication.addBeanFactoryPostProcessor();
-//        MSF4JSpringApplication msf4JSpringApplication = new MSF4JSpringApplication(ProductResource.class);
-//        ConfigurableApplicationContext configurableApplicationContext =
-//                MSF4JSpringApplication.run(ProductResource.class, "--http.port=8090");
-//        msf4JSpringApplication.addService(configurableApplicationContext, ProductResource.class,
-//                "/reception-service")
-//                .addGlobalRequestInterceptor(new CustomJWTClaimsFeature());
+        context.getBean(SpringMicroservicesRunner.class)
+                .addGlobalRequestInterceptor(new ReqInterceptor());
     }
 }
